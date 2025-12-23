642fff1e642cedd370fe4035cd7c395ad71f6168
Pankaj900-ops <142712027+Pankaj900-ops@users.noreply.github.com>
2025-12-23 21:30:35 +0530

    eight

--- Qtify-main/src/components/NavBar/NavBar.jsx
+++ Qtify-main/src/components/NavBar/NavBar.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import styles from "./NavBar.module.css";
+import BrandLogo from "../../assets/BrandLogo.png";
 
 const NavBar = () => {
   return (
     <div className={styles.navBar}>
       {/* Logo */}
       <img
-        src="/logo.png"
+       src={BrandLogo}
         alt="logo"
         className={styles.logo}
       />
