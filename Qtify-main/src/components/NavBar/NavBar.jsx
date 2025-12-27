703fc890649505906181e12227999895fc77954a
Pankaj900-ops <142712027+Pankaj900-ops@users.noreply.github.com>
2025-12-27 20:26:19 +0530

    eleven

--- Qtify-main/src/components/NavBar/NavBar.jsx
+++ Qtify-main/src/components/NavBar/NavBar.jsx
@@ -4,27 +4,23 @@ import BrandLogo from "../../assets/BrandLogo.png";
 
 const NavBar = () => {
   return (
-    <div className={styles.navBar}>
-      {/* Logo */}
+    <div className={styles.navbar}>
       <img
-       src={BrandLogo}
+        src={BrandLogo}
         alt="logo"
         className={styles.logo}
       />
 
-      {/* Search Bar */}
       <input
         type="text"
         placeholder="Search a song"
         className={styles.search}
       />
 
-      {/* Button */}
       <button className={styles.feedbackBtn}>
         Give Feedback
       </button>
     </div>
-    
   );
 };
 
